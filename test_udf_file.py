bb025e26adc3ea27d18e299f4fc2a3d082c4ef5c
Prakash Loganathan <prakash.loganathaan@gmail.com>
2023-12-13 09:57:39 -0500

    Snowpark learning scripts

--- test_udf_file.py
+++ test_udf_file.py
@@ -1,2 +1,11 @@
+'''
+Function definition:
+def mod5(x: int) -> int:
+--
+def: This keyword signifies the beginning of a function definition.
+mod5: This is the name of the function.
+(x: int): This specifies the parameter of the function. Here, it expects an integer as input and names it x.
+-> int: This specifies the return type of the function. Here, it indicates that the function will return an integer.
+'''
 def mod5(x: int) -> int:
     return x % 5
